00a19f3f5d8cf0db49a94c796c228c4edc222e39
Havoc Pennington <hp@pobox.com>
2011-11-28 12:52:44 -0500

    fix a typo in a comment in simple-lib

--- examples/simple-lib/src/main/scala/simplelib/SimpleLib.scala
+++ examples/simple-lib/src/main/scala/simplelib/SimpleLib.scala
@@ -7,7 +7,7 @@ class SimpleLibContext(config: Config) {
 
     // This verifies that the Config is sane and has our
     // reference config. Importantly, we specify the "simple-lib"
-    // path so we only validate settings that belong to our this
+    // path so we only validate settings that belong to this
     // library. Otherwise, we might throw mistaken errors about
     // settings we know nothing about.
     config.checkValid(ConfigFactory.defaultReference(), "simple-lib")
