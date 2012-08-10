ac60eae2a0926db351576de45db7fb27d3ae5739
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-08-10 12:15:37 -0500

    left-associative: test with list and test with object

--- config/src/test/java/com/typesafe/config/impl/ArrayWithObjectTest.java
+++ config/src/test/java/com/typesafe/config/impl/ArrayWithObjectTest.java
@@ -116,7 +116,7 @@ public class ArrayWithObjectTest {
 	@Test
 	public void test7() {
 
-		final String text = "{ item = { a:1, b:2 } , list = [ { a:2} ] ${item}  }";
+		final String text = "{ item = { a:1, b:2 } , list = [ { a:2 } ] ${item}  }";
 
 		Config config = ConfigFactory.parseString(text).resolve();
 
@@ -126,6 +126,48 @@ public class ArrayWithObjectTest {
 		Config entry = config(list.get(0));
 		assertEquals("provided a", entry.getNumber("a"), 2);
 		assertEquals("default  b", entry.getNumber("b"), 2);
+
+	}
+
+	/** left-associative: fall-back to object then concatenate with list */
+	@Test
+	public void test8() {
+
+		final String text = "{ list = [ { a:1 } ] { b:2 } [ { c:3 } ] }";
+
+		Config config = ConfigFactory.parseString(text);
+
+		log(config);
+
+		ConfigList list = config.getList("list");
+		assertEquals("two item list", list.size(), 2);
+
+		Config entry0 = config(list.get(0));
+		assertEquals("provided a", entry0.getNumber("a"), 1);
+		assertEquals("default  b", entry0.getNumber("b"), 2);
+
+		Config entry1 = config(list.get(1));
+		assertEquals("concat   c", entry1.getNumber("c"), 3);
+
+	}
+
+	/** left-associative: fall-back to object 1 then fall-back to object 2 */
+	@Test
+	public void test9() {
+
+		final String text = "{ list = [ { a:1 } ] { b:2 } { c:3 } }";
+
+		Config config = ConfigFactory.parseString(text);
+
+		log(config);
+
+		ConfigList list = config.getList("list");
+		assertEquals("one item list", list.size(), 1);
+
+		Config entry = config(list.get(0));
+		assertEquals("provided a", entry.getNumber("a"), 1);
+		assertEquals("object 1 b", entry.getNumber("b"), 2);
+		assertEquals("object 2 c", entry.getNumber("c"), 3);
 
 	}
 
