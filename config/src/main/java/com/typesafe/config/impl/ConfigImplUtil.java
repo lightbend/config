d3f638f2c0d7385465e221d215e22a16b2bcbe34
Havoc Pennington <hp@pobox.com>
2013-09-19 17:37:04 -0400

    Unquote keys with hyphen when rendering, fixes #83

--- config/src/main/java/com/typesafe/config/impl/ConfigImplUtil.java
+++ config/src/main/java/com/typesafe/config/impl/ConfigImplUtil.java
@@ -78,8 +78,10 @@ final public class ConfigImplUtil {
         if (s.length() == 0)
             return renderJsonString(s);
 
+        // if it starts with a hyphen or number, we have to quote
+        // to ensure we end up with a string and not a number
         int first = s.codePointAt(0);
-        if (Character.isDigit(first))
+        if (Character.isDigit(first) || first == '-')
             return renderJsonString(s);
 
         if (s.startsWith("include") || s.startsWith("true") || s.startsWith("false")
@@ -89,7 +91,7 @@ final public class ConfigImplUtil {
         // only unquote if it's pure alphanumeric
         for (int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
-            if (!(Character.isLetter(c) || Character.isDigit(c)))
+            if (!(Character.isLetter(c) || Character.isDigit(c) || c == '-'))
                 return renderJsonString(s);
         }
 
