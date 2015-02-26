db5e95c457a8c900ed8315f26e09bc8e1c00d1f8
Havoc Pennington <hp@pobox.com>
2015-02-26 09:17:41 -0500

    Fix ConfigMemorySizeTest.scala indentation

--- config/src/test/scala/com/typesafe/config/impl/ConfigMemorySizeTest.scala
+++ config/src/test/scala/com/typesafe/config/impl/ConfigMemorySizeTest.scala
@@ -9,17 +9,17 @@ import com.typesafe.config.ConfigMemorySize
 
 class ConfigMemorySizeTest extends TestUtils {
 
-  @Test
-  def testEquals() {
-    assertTrue("Equal ConfigMemorySize are equal",
-               ConfigMemorySize.ofBytes(10).equals(ConfigMemorySize.ofBytes(10)))
-    assertTrue("Different ConfigMemorySize are not equal",
-               !ConfigMemorySize.ofBytes(10).equals(ConfigMemorySize.ofBytes(11)))
-  }
+    @Test
+    def testEquals() {
+        assertTrue("Equal ConfigMemorySize are equal",
+            ConfigMemorySize.ofBytes(10).equals(ConfigMemorySize.ofBytes(10)))
+        assertTrue("Different ConfigMemorySize are not equal",
+            !ConfigMemorySize.ofBytes(10).equals(ConfigMemorySize.ofBytes(11)))
+    }
 
-  @Test
-  def testToUnits() {
-    val kilobyte = ConfigMemorySize.ofBytes(1024)
-    assertEquals(1024, kilobyte.toBytes)
-  }
+    @Test
+    def testToUnits() {
+        val kilobyte = ConfigMemorySize.ofBytes(1024)
+        assertEquals(1024, kilobyte.toBytes)
+    }
 }
