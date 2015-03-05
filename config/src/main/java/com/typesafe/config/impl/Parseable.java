52ba150a9a67226bbc095f33a1b7cc94935e26b2
Havoc Pennington <hp@pobox.com>
2015-03-04 21:12:03 -0500

    Add HttpTest for testing URL parsing and error handling

--- config/src/main/java/com/typesafe/config/impl/Parseable.java
+++ config/src/main/java/com/typesafe/config/impl/Parseable.java
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
