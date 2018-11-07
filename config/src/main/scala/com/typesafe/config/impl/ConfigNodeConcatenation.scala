bd8afbfe045cc253b61d8ab4209ba1922b963e7d
Eric Richardson <ekrichardson@gmail.com>
2018-11-07 12:01:47 -0500

    Convert ConfigNodeConcatenation

---
+++ config/src/main/scala/com/typesafe/config/impl/ConfigNodeConcatenation.scala
@@ -0,0 +1,10 @@
+package com.typesafe.config.impl
+
+import java.{util=>ju}
+
+final class ConfigNodeConcatenation private[impl] (
+                                                    children: ju.Collection[AbstractConfigNode]
+                                                  ) extends ConfigNodeComplexValue(children) {
+  override def newNode(nodes: ju.Collection[AbstractConfigNode]) =
+    new ConfigNodeConcatenation(nodes)
+}
