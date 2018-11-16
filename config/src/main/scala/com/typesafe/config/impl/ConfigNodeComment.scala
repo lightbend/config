50ba197856d5b24c20f5a92aa9a2d36ea8c0e06d
Eric Richardson <ekrichardson@gmail.com>
2018-11-16 15:07:17 -0500

    Convert ConfigNodeComment

---
+++ config/src/main/scala/com/typesafe/config/impl/ConfigNodeComment.scala
@@ -0,0 +1,13 @@
+package com.typesafe.config.impl
+
+import com.typesafe.config.ConfigException
+
+final class ConfigNodeComment(val comment: Token)
+    extends ConfigNodeSingleToken(comment) {
+
+    if (!Tokens.isComment(token))
+        throw new ConfigException.BugOrBroken(
+            "Tried to create a ConfigNodeComment from a non-comment token")
+
+    protected def commentText: String = Tokens.getCommentText(token)
+}
