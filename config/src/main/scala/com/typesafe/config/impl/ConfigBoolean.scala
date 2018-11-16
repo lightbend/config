afb7b330f781fcdf2e1024202bed9b67ba778fcd
Eric Richardson <ekrichardson@gmail.com>
2018-11-16 14:39:11 -0500

    Convert ConfigBoolean

---
+++ config/src/main/scala/com/typesafe/config/impl/ConfigBoolean.scala
@@ -0,0 +1,30 @@
+/**
+ *   Copyright (C) 2011-2012 Typesafe Inc. <http://typesafe.com>
+ */
+package com.typesafe.config.impl
+
+import java.{ lang => jl }
+import java.io.ObjectStreamException
+import java.io.Serializable
+import com.typesafe.config.ConfigOrigin
+import com.typesafe.config.ConfigValueType
+
+@SerialVersionUID(2L)
+final class ConfigBoolean(
+    origin: ConfigOrigin,
+    val value: Boolean)
+    extends AbstractConfigValue(origin)
+    with Serializable {
+
+    override def valueType: ConfigValueType = ConfigValueType.BOOLEAN
+
+    override def unwrapped: Boolean = value
+
+    override def transformToString: String = if (value) "true" else "false"
+
+    override def newCopy(origin: ConfigOrigin) = new ConfigBoolean(origin, value)
+
+    // serialization all goes through SerializedConfigValue
+    @throws[ObjectStreamException]
+    private def writeReplace(): jl.Object = new SerializedConfigValue(this)
+}
