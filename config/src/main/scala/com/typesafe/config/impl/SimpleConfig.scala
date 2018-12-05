805de43cfecb8f30f2d3fe60435ba0c0b23b7db8
Eric Richardson <ekrichardson@gmail.com>
2018-12-05 08:39:24 -0500

    Fix for Java example applications

--- config/src/main/scala/com/typesafe/config/impl/SimpleConfig.scala
+++ config/src/main/scala/com/typesafe/config/impl/SimpleConfig.scala
@@ -768,17 +768,15 @@ final class SimpleConfig private[impl] (val `object`: AbstractConfigObject)
     override def toString: String = "Config(" + `object`.toString + ")"
     private def peekPath(path: Path): AbstractConfigValue = root.peekPath(path)
     override def isResolved: Boolean = root.resolveStatus eq ResolveStatus.RESOLVED
-    // This method in Config uses @varargs so it can be called from
-    // Java with varargs
+
+    // This method in Config uses @varargs so it can be called from Java with varargs
     // See https://github.com/scala/bug/issues/10658
     // originally: @Override public void checkValid(Config reference, String... restrictToPaths)
-    // Now the code goes through the Scala varargs method and this one is not needed.
-    //    override def checkValid(reference: Config, restrictToPaths: Seq[String]): Unit = {
-    //        val javaListPaths = JavaConverters.asJavaListConverter(restrictToPaths).asJava
-    //        val javaArrayPaths = javaListPaths.toArray(new Array[String](0))
-    //        checkValid(reference, restrictToPaths: _*)
-    //    }
-    // Comment can be removed but here for info purposes
+    // Now the code goes through the Scala varargs method but we need this one for Java
+    def checkValid(reference: Config, restrictToPaths: Array[String]): Unit = {
+        checkValid(reference, restrictToPaths: _*)
+    }
+
     override def checkValid(reference: Config, restrictToPaths: String*): Unit = {
         val ref = reference.asInstanceOf[SimpleConfig]
         // unresolved reference config is a bug in the caller of checkValid
