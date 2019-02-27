1622208ebd0ed2ea48261c49ba194f9bb2db5b7f
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2019-02-27 12:37:37 +0000

    Add a config strategy to load from environment variables first.

--- config/src/test/scala/com/typesafe/config/impl/ConfigTest.scala
+++ config/src/test/scala/com/typesafe/config/impl/ConfigTest.scala
@@ -1091,6 +1091,53 @@ class ConfigTest extends TestUtils {
     }
 
     @Test
+    def testLoadWithEnvSubstitutions() {
+        TestEnvFirstStrategy.putEnvVar("CONFIG_42___a", "1")
+        TestEnvFirstStrategy.putEnvVar("CONFIG_a_b_c", "2")
+        TestEnvFirstStrategy.putEnvVar("CONFIG_a__c", "3")
+        TestEnvFirstStrategy.putEnvVar("CONFIG_a___c", "4")
+
+        TestEnvFirstStrategy.putEnvVar("CONFIG_akka_version", "foo")
+        TestEnvFirstStrategy.putEnvVar("CONFIG_akka_event__handler__dispatcher_max__pool__size", "10")
+
+        System.setProperty("config.strategy", classOf[EnvFirstConfigLoadingStrategy].getCanonicalName)
+
+        try {
+            val loader02 = new TestClassLoader(this.getClass().getClassLoader(),
+                Map("reference.conf" -> resourceFile("test02.conf").toURI.toURL()))
+
+            val loader04 = new TestClassLoader(this.getClass().getClassLoader(),
+                Map("reference.conf" -> resourceFile("test04.conf").toURI.toURL()))
+
+            val conf02 = withContextClassLoader(loader02) {
+                ConfigFactory.load()
+            }
+
+            val conf04 = withContextClassLoader(loader04) {
+                ConfigFactory.load()
+            }
+
+            assertEquals(1, conf02.getInt("42_a"))
+            assertEquals(2, conf02.getInt("a.b.c"))
+            assertEquals(3, conf02.getInt("a-c"))
+            assertEquals(4, conf02.getInt("a_c"))
+
+            assertEquals("foo", conf04.getString("akka.version"))
+            assertEquals(10, conf04.getInt("akka.event-handler-dispatcher.max-pool-size"))
+        } finally {
+            System.clearProperty("config.strategy")
+
+            TestEnvFirstStrategy.removeEnvVar("CONFIG_42___a")
+            TestEnvFirstStrategy.removeEnvVar("CONFIG_a_b_c")
+            TestEnvFirstStrategy.removeEnvVar("CONFIG_a__c")
+            TestEnvFirstStrategy.removeEnvVar("CONFIG_a___c")
+
+            TestEnvFirstStrategy.removeEnvVar("CONFIG_akka_version")
+            TestEnvFirstStrategy.removeEnvVar("CONFIG_akka_event__handler__dispatcher_max__pool__size")
+        }
+    }
+
+    @Test
     def renderRoundTrip() {
         val allBooleans = true :: false :: Nil
         val optionsCombos = {
