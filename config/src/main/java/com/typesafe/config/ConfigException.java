431e0ca33dddd699172e9372a3fc8b13b17bfbe5
kringol <kringol@gmail.com>
2019-10-04 11:03:29 +0900

    Change BadKey exception to extend from ConfigException instead

--- config/src/main/java/com/typesafe/config/ConfigException.java
+++ config/src/main/java/com/typesafe/config/ConfigException.java
@@ -446,8 +446,13 @@ public abstract class ConfigException extends RuntimeException implements Serial
      * Exception indicating that a key was messed up, for example you may have
      * asked for an Enum and the key text value does not exist on the requested Enum.
      */
-    public static class BadKey extends BugOrBroken {
+    public static class BadKey extends ConfigException {
         private static final long serialVersionUID = 1L;
+
+        public BadKey(ConfigOrigin origin, String path, String message,
+                        Throwable cause) {
+            super(origin, "Invalid key at '" + path + "': " + message, cause);
+        }
 
         public BadKey(String message, Throwable cause) {
             super(message, cause);
