09e2d57e18aaa59af3308da61cba61df8846073f
Brian Baldino <bbaldino@gmail.com>
2020-09-22 15:42:58 -0700

    Split out a helper method for parsing only application overrides

--- config/src/main/java/com/typesafe/config/ConfigFactory.java
+++ config/src/main/java/com/typesafe/config/ConfigFactory.java
@@ -8,7 +8,7 @@ import com.typesafe.config.impl.Parseable;
 
 import java.io.File;
 import java.io.Reader;
-import java.net.URL;
+import java.net.*;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Callable;
@@ -1088,6 +1088,63 @@ public final class ConfigFactory {
      */
     public static Config parseResourcesAnySyntax(String resourceBasename) {
         return parseResourcesAnySyntax(resourceBasename, ConfigParseOptions.defaults());
+    }
+
+    /**
+     * Parse only any application overrides (those specified by config.{resource,file,url}), returning
+     * an empty Config if no overrides were set.
+     * @param parseOptions parse options
+     * @return the parsed configuration
+     */
+    public static Config parseApplicationOverride(ConfigParseOptions parseOptions) {
+        ClassLoader loader = parseOptions.getClassLoader();
+
+        if (loader == null)
+            throw new ConfigException.BugOrBroken(
+                "ClassLoader should have been set here; bug in ConfigFactory. "
+                    + "(You can probably work around this bug by passing in a class loader or calling currentThread().setContextClassLoader() though.)");
+
+        int specified = 0;
+
+        // override application.conf with config.file, config.resource,
+        // config.url if requested.
+        String resource = System.getProperty("config.resource");
+        if (resource != null)
+            specified += 1;
+        String file = System.getProperty("config.file");
+        if (file != null)
+            specified += 1;
+        String url = System.getProperty("config.url");
+        if (url != null)
+            specified += 1;
+
+        if (specified == 0) {
+            return ConfigImpl.emptyConfig("TODO: what to put here? Should something else be returned?");
+        } else if (specified > 1) {
+            throw new ConfigException.Generic("You set more than one of config.file='" + file
+                + "', config.url='" + url + "', config.resource='" + resource
+                + "'; don't know which one to use!");
+        } else {
+            // the override file/url/resource MUST be present or it's an error
+            ConfigParseOptions overrideOptions = parseOptions.setAllowMissing(false);
+            if (resource != null) {
+                if (resource.startsWith("/"))
+                    resource = resource.substring(1);
+                // this deliberately does not parseResourcesAnySyntax; if
+                // people want that they can use an include statement.
+                return ConfigFactory.parseResources(loader, resource, overrideOptions);
+            } else if (file != null) {
+                return ConfigFactory.parseFile(new File(file), overrideOptions);
+            } else {
+                try {
+                    return ConfigFactory.parseURL(new URL(url), overrideOptions);
+                } catch (MalformedURLException e) {
+                    throw new ConfigException.Generic("Bad URL in config.url system property: '"
+                        + url + "': " + e.getMessage(), e);
+                }
+            }
+        }
+
     }
 
     /**
